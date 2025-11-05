34a490d47ae21384956b1108d65e4bf17880d7ad
Kaiwen Yao <kw.yao@yandex.com>
2025-11-05 20:21:52 +0000

    完成图表 下一步是自主实现班级

--- tlias-web-management/src/main/java/com/itheima/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -2,13 +2,11 @@ package com.itheima.mapper;
 
 import com.itheima.pojo.Emp;
 import com.itheima.pojo.EmpQueryParam;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface EmpMapper {
@@ -39,4 +37,9 @@ public interface EmpMapper {
     Emp getById(Integer id);
 
     void updateById(Emp emp);
+
+    @MapKey("pos")
+    List<Map<String, Object>> countEmpJobData();
+    @MapKey("name")
+    List<Map<String, Object>> countEmpGenderData();
 }
