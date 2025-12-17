fc4bc3e540727fae16748a14efefbf3cafa20fa8
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-12-17 10:01:38 +0000

    包名修改

--- tlias-web-management/src/main/java/com/itheima/config/WebConfig.java
+++ tlias-web-management/src/main/java/com/itheima/config/WebConfig.java
@@ -9,15 +9,15 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-/*    @Autowired
-    private DemoIntercepter demoIntercepter;
-    @Autowired
-    private TokenIntercepter tokenIntercepter;
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenIntercepter).addPathPatterns("/**").excludePathPatterns(
-                "/login");
-
-    }*/
+//    @Autowired
+//    private DemoIntercepter demoIntercepter;
+//    @Autowired
+//    private TokenIntercepter tokenIntercepter;
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(tokenIntercepter).addPathPatterns("/**").excludePathPatterns(
+//                "/login");
+//
+//    }
 
 }
